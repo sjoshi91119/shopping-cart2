1fd71d4454637b374412bfe2a2468c4b66a795ed
Shubham Joshi <sjoshi91119@gmail.com>
2023-07-02 00:24:49 +0530

    gh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import HomePage from './Components/HomePage';
-import CartPage from './Components/CartPage';
-import {Routes,Route} from 'react-router-dom';
-import './Components/styles.css'
-import Nav from './Components/Nav';
+import "./App.css";
+import React from "react";
+import HomePage from "./Components/HomePage";
+import CartPage from "./Components/CartPage";
+import { Routes, Route } from "react-router-dom";
+import "./Components/styles.css";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
-   <div>
-    <Nav/>
-    <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/cart' element={<CartPage/>}/>
-    </Routes>
-   </div>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </div>
   );
 }
 
